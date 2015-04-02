79773819838fc15840b18c24b6d375e863761c70
rodav2 <guerrydavid@orange.fr>
2015-04-02 15:35:09 +0200

    Revert "twitter"
    This reverts commit dd6abe61900c85da41fd979dd9b6948e6859524a.

--- ProjetPHPv1/Deconnexion.php
+++
@@ -1,9 +0,0 @@
-<!--
-AUTEUR: Guerry David
-DATE DE DEBUT: 13-12-2014
-PROJET LicencePro twitter-->
-
-<?php		
-	session_unset(); // Détruit toutes les variables d'une session
-	header('Location:index.php'); // Redirection vers la page index.php
-?>
